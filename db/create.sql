2225ebd42e025cae2e89eb7dde68fc3c991670b2
Sekator778 <sekator778@gmail.com>
2020-04-18 21:47:36 +0300

    Sql схема для проекта Tracker.[#276988]

---
+++ db/create.sql
@@ -0,0 +1,5 @@
+create table items
+(
+    id   serial primary key not null,
+    name varchar(2000)
+);
---
+++ db/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO items (name)
+VALUES ('one item'),
+       ('two item'),
+       ('three item'),
+       ('four item'),
+       ('five item');
