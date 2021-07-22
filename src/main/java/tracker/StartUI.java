9267fa7cb77288fc608984cd42bccc7aad3f1a93
Sekator778 <sekator778@gmail.com>
2021-07-22 16:07:10 +0300

    1

--- src/main/java/tracker/StartUI.java
+++ src/main/java/tracker/StartUI.java
@@ -27,25 +27,25 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Item item = new Item("Item xxx");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println("Текущие дата и время создания объекта: " + item.getCreated().format(formatter));
-//        Input input = new ConsoleInput();
-//        Consumer<String> stringConsumer = System.out::println;
-//        Input validateInput = new ValidateInput(input, stringConsumer);
-//        try (Store tracker = new TrackerSQL()) {
-//            tracker.init();
-//            List<UserAction> actions = new ArrayList<>();
-//            actions.add(new CreateAction());
-//            actions.add(new ShowAllAction());
-//            actions.add(new ReplaceItemAction());
-//            actions.add(new DeleteAction());
-//            actions.add(new FindItemByIdAction());
-//            actions.add(new FindByNameAction());
-//            actions.add(new EndAction());
-//            new StartUI().init(validateInput, tracker, actions, stringConsumer);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+//        Item item = new Item("Item xxx");
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+//        System.out.println("Текущие дата и время создания объекта: " + item.getCreated().format(formatter));
+        Input input = new ConsoleInput();
+        Consumer<String> stringConsumer = System.out::println;
+        Input validateInput = new ValidateInput(input, stringConsumer);
+        try (Store tracker = new TrackerSQL()) {
+            tracker.init();
+            List<UserAction> actions = new ArrayList<>();
+            actions.add(new CreateAction());
+            actions.add(new ShowAllAction());
+            actions.add(new ReplaceItemAction());
+            actions.add(new DeleteAction());
+            actions.add(new FindItemByIdAction());
+            actions.add(new FindByNameAction());
+            actions.add(new EndAction());
+            new StartUI().init(validateInput, tracker, actions, stringConsumer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
